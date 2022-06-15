cd444a52451c3e80949af629ba3e5ec7cce7178e
ArunMozhi-Maruthamuthu <107558733+ArunMozhi-Maruthamuthu@users.noreply.github.com>
2022-06-15 20:52:13 +0530

    Add files via upload

---
+++ Create Challenge1-ThreetierEnv/variablesdeclare.tf
@@ -0,0 +1,15 @@
+variable "rgname"{
+    description = "Name of the resourcegroup"
+}
+
+variable "location"{
+    description = "Location of the services"
+}
+
+variable "project"{
+    description = "Name of the project"
+}
+
+variable "environment"{
+    description = "Name of the environment"
+}
